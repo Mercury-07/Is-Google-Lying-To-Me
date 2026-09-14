6c6b251f51bbdd803db3b971951277656ec9aea5
Mercury-07 <grahamhudson84@gmail.com>
2026-09-14 19:53:35 -0400

    confirmed, google lies

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 
 import os
+import math
 
-onFile = os.path.getsize(r"C:\Users\Hudson\Desktop\cis2500\Practice\Uno\add.c")
+sum = 0
+leader = 0
+leader_path = ""
+drive_path = r"G:\My Drive"
 
-print(onFile)
+for root, dirs, files in os.walk(drive_path):
+    for file in files:
+        currentSize = os.path.getsize(os.path.join(root, file))
+        sum += currentSize
+        if(leader < currentSize): 
+            leader = currentSize
+            leader_path = os.path.join(root, file)
 
-testSize = os.path.getsize(r"G:\My Drive\1160 - Pie chart 1.gsheet")
+print("Sum in bytes:", sum)
+print("Sum in gb:", sum/10**9)
 
-print(testSize)
-
-
+print("Leader: ", leader_path )
+print("Size: ", leader)
 
 
---
+++ os_documentation.py
@@ -0,0 +1,13 @@
+"""
+
+os.path.abspath("Keyword")
+returns a string of current path with the keyword on the end
+
+
+os.path.isfile(path)
+Returns true is the path is a regular file. Returns false if folder
+
+
+
+
+"""
