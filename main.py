c78b37058ba101b8467b565a25d114d0c335f95c
Mercury-07 <grahamhudson84@gmail.com>
2026-09-14 19:08:50 -0400

    os tests

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-#Git test
 
+import os
+
+onFile = os.path.getsize(r"C:\Users\Hudson\Desktop\cis2500\Practice\Uno\add.c")
+
+print(onFile)
+
+testSize = os.path.getsize(r"G:\My Drive\1160 - Pie chart 1.gsheet")
+
+print(testSize)
 
 
 
